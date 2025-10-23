61dc6ec2951dabd8e4d9c6c845631e74057a76b6
Max Clayton Clowes <contact@mcclowes.com>
2025-10-23 15:36:47 +0100

    Fix deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,17 @@
-
-
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@chromatic-com/storybook',
-    '@storybook/addon-docs',
     '@storybook/addon-a11y',
+    '@storybook/addon-docs',
     '@storybook/addon-vitest',
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+  docs: {
+    autodocs: 'tag',
   },
 };
 
